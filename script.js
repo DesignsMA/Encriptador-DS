c366c883c32861d402a2a5bbf7799bbb172ec239
DesignMA <designsma.contact@gmail.com>
2023-07-23 22:13:24 -0600

    Agregando estilos

---
+++ script.js
@@ -0,0 +1,23 @@
+const btnEncriptar = document.querySelector('[data-form-btn-do]')
+const btnDesencriptar = document.querySelector('[data-form-btn-redo]')
+
+const input = "Habia Una Vez Una Persona con muchos huevuis"
+let resultado = input.toLowerCase();
+console.log(resultado);
+
+const encriptar = (evento) => {
+    evento.preventDefault();
+    resultado = resultado.replace(/u/g, "str");
+    console.log(resultado);
+}
+
+const desencriptar = (evento) => {
+    evento.preventDefault();
+    resultado = resultado.replace(/str/g, "u");
+    console.log(resultado);
+}
+
+
+btnEncriptar.addEventListener('click', encriptar)
+btnDesencriptar.addEventListener('click', desencriptar)
+
